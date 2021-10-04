903c11505f99ecf4dced8b3524e92828a4eaa72c
Jacqueline Marques <jaquecdp@gmail.com>
2021-10-04 15:50:53 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Aprendendo o básico do HTML</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="estilo.css"
+  </head>
+  <body style="background-color:blue; color:white; text-align:center; font-size: 50px;">
+     hello world 
+  </body>
+</html>
